3a103e50baedc5c966542ec8ad94262604a44e43
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-20 15:01:56 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,13 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      text: "#011C26",
+      textSecondary: "#084A59",
+      white: "#FFFFFF",
+      orange:"#BF522A",
+      yellow: "#F2B138",
+      gray: "gray"
     },
   },
   plugins: [],
