3ce003c287dc4fcc82d42f0bdd486092a42fc08a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-20 16:42:12 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 
 export default function Home() {
   return (
     <>
     <NavBar />
     <main className="">
-
+      <Hero />
     </main>
     </>
   )
