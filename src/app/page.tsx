abffa95bc3edccdd00ad28454491e6e28c442776
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-20 15:01:54 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import NavBar from './components/NavBar'
 
 export default function Home() {
   return (
+    <>
+    <NavBar />
     <main className="">
-     <h1 className='text-red-500'>Bonjour</h1>
+
     </main>
+    </>
   )
 }
