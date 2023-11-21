c0f40a4291f861883f20c1e0be773eee85db4228
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-21 07:50:47 +0100

    Update Qui.tsx

--- src/app/components/Qui.tsx
+++ src/app/components/Qui.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import Ligne from "./Ligne";
-import { MapIcon } from '@heroicons/react/24/solid';
+import { MapPinIcon } from '@heroicons/react/24/solid';
 
 const cards =[
     {
         title: "Proximité",
-        text: "Trouvez vos chantier grace à des Propsects qualifiés,  des projets sur-mesure, clés en main sans engagement ni frai de service. Je suis un véritable artisan du web au service de mes clients. ",
+        text: "En tant qu'artisan du web, je m'engage à vous fournir des prospects qualifiés et des projets sur-mesure dans le bâtiment, garantissant une collaboration étroite et sans engagement ni frais de service.",
     },
     {
         title: "Efficacité", 
-        text: "Trouvez vos chantier grace à des Propsects qualifiés,  des projets sur-mesure, clés en main sans engagement ni frai de service. Je suis un véritable artisan du web au service de mes clients. "
+        text: "Je maximise l'efficacité de vos campagnes avec des prospects ciblés et des solutions clés en main dans le bâtiment, le tout en tant qu'artisan du web dédié et sans contraintes de service."
     },
     {
         title: "Rentabilité",
-        text: "Trouvez vos chantier grace à des Propsects qualifiés,  des projets sur-mesure, clés en main sans engagement ni frai de service. Je suis un véritable artisan du web au service de mes clients. "
+        text: "Augmentez votre rentabilité grâce à mes services personnalisés dans le bâtiment, offrant des prospects qualifiés et des campagnes efficaces, sans engagement ni frais de service."
     }
 ]
 
@@ -21,7 +21,7 @@ const cards =[
 const Qui: React.FC = () => {
     return (
         <section className="">
-            <div className="mx-auto flex max-w-7xl p-6 lg:px-8 lg:pt-32 pt-20 md:flex-row flex-col justify-between items-center bg-white">
+            <div className="mx-auto flex max-w-5xl p-6 lg:px-8 lg:pt-32 pt-20 md:flex-row flex-col justify-between items-center bg-white">
                 <div className="md:w-6/12 w-full text-text">
                     <h2 className="text-2xl font-bold">Qui suis-je ?</h2>
                     <div className="mt-7">
@@ -30,23 +30,22 @@ const Qui: React.FC = () => {
                     <div className='w-full flex justify-between py-10'>
                         <p>Nicolas CASTERA</p>
                         <div className='flex'>
-                            <MapIcon className="h-5 w-5 text-yellow" aria-hidden="true" />
-                            <p className='ml-4'>Bordeaux</p>
+                            <MapPinIcon className="h-5 w-5 text-yellow" aria-hidden="true" />
+                            <p className='ml-2'>Bordeaux</p>
                         </div>
                     </div>
-                    <p>Trouvez vos chantier grace à des Propsects qualifiés,  des projets sur-mesure, clés en main sans engagement ni frai de service. Je suis un véritable artisan du web au service de mes clients. Trouvez vos chantier grace à des Propsects qualifiés,  des projets sur-mesure, clés en main sans engagement ni frai de service. Je suis un véritable artisan du web au service de mes clients. Trouvez vos chantier grace à des Propsects qualifiés,  des projets sur-mesure, clés en main sans engagement ni frai de service. Je suis un véritable artisan du web au service de mes clients. Trouvez vos chantier grace à des Propsects qualifiés,  des projets sur-mesure, clés en main sans engagement ni frai de service. Je suis un véritable artisan du web au service de mes clients. Trouvez vos chantier grace à des Propsects qualifiés,  des projets sur-mesure, clés en main sans engagement ni frai de service. Je suis un véritable artisan du web au service de mes clients. </p>
-                    <p className='mt-7'>Trouvez vos chantier grace à des Propsects qualifiés,  des projets sur-mesure, clés en main sans engagement ni frai de service. Je suis un véritable artisan du web au service de mes clients. </p>
+                    <p>Expert en marketing digital et communication en ligne, basé à Bordeaux. Fort de plus de 10 ans d'expérience, je me spécialise dans les campagnes Google Ads et la création de landing pages sur-mesure pour les professionnels du bâtiment. Mon approche est centrée sur l'acquisition de prospects qualifiés et l'optimisation des campagnes pour une efficacité maximale. Je m'adapte aux besoins spécifiques de chaque client, offrant une flexibilité budgétaire et une transparence totale. Mon objectif est d'accroître significativement le chiffre d'affaires de mes clients dans le bâtiment, en garantissant des résultats concrets et un taux de conversion élevé, le tout sans engagement ni frais de service.</p>
                 </div>
                 <div className="md:w-6/12 w-full flex justify-center items-center md:mt-0 mt-12">
                     <img src="/img/moi2.png" className="max-h-[500px] h-auto" alt="Image Description" />
                 </div>
             </div>
-            <div className="mx-auto flex max-w-7xl p-6 lg:px-8 lg:pb-32 pb-20 md:flex-row flex-col justify-between items-center bg-white mt-16">
+            <div className="mx-auto flex max-w-5xl p-6 lg:px-8 lg:pb-32 pb-20 md:flex-row flex-col justify-between items-center bg-white mt-16">
                 {
                     cards.map((card, index) => (
-                        <div className='md:w-4/12 w-full flex flex-col items-center md:max-w-[30%] p-7 text-white bg-textSecondary rounded-md md:mt-0 mt-7' key={index}>
+                        <div className='md:w-4/12 w-full flex flex-col items-center md:max-w-[30%] min-h-[340px] p-7 text-white bg-textSecondary rounded-md md:mt-0 mt-7' key={index}>
                             <h3 className='text-xl font-bold mt-4'>{card.title}</h3>
-                            <p className='text-center mt-4'>{card.text}</p>
+                            <p className='text-center mt-4'>{card.text}</p> 
                         </div>
                     ))
                 }
