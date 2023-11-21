a7c9645589217cb38aa1bb2476dcd42f6bdab07e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-21 07:50:45 +0100

    Update NavBar.tsx

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -10,7 +10,7 @@ const navigation = [
 const NavBar = () => {
     return (
         <header className='text-text'>
-            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+            <nav className="mx-auto flex max-w-5xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex">
                     <a href="#" className="-m-1.5 p-1.5">
                         <span className="sr-only">Trouver-mon-chantier</span>
