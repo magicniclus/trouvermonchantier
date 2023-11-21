e70a0809c2573f61fdd0bdc582fc3320e4ae6b58
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-21 11:45:32 +0100

    Update ButtonAction.tsx

--- src/app/components/ButtonAction.tsx
+++ src/app/components/ButtonAction.tsx
@@ -10,7 +10,7 @@ interface ButtonPhoneProps {
 const ButtonPhone: React.FC<ButtonPhoneProps> = ({ value = 'Envoyer', bgColor = "textSecondary", textColor = "white" }) => {
 
     return (
-        <a href="#form" className={`py-3 lg:px-5 px-3 rounded-lg bg-${bgColor} text-${textColor} flex mt-10 min-w-[150px] justify-center items-center`}>
+        <a href="#form" className={`py-3 lg:px-5 px-3 rounded-lg bg-${bgColor} text-${textColor} flex mt-10 min-w-[150px] justify-center items-center hover:transition-all hover:duration-100 hover:ease-in-out hover:shadow-lg`}>
             <div className='flex'>
                 {value}
             </div>
