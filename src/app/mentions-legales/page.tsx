939a53e547bf75310b5d1de44cdb1b8ec96d16d6
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-21 14:24:33 +0100

    Update page.tsx

--- src/app/mentions-legales/page.tsx
+++ src/app/mentions-legales/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 
 const page = () => {
     return (
@@ -217,6 +218,7 @@ const page = () => {
         </p>
       </div>
             </main>
+            <Footer/>
         </>
     );
 };
