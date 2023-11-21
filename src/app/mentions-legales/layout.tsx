71c213faab3253b5ad78bd34aed3649ff084ae5a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-21 14:26:32 +0100

    Update layout.tsx

--- src/app/mentions-legales/layout.tsx
+++ src/app/mentions-legales/layout.tsx
@@ -5,8 +5,8 @@ import './../globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Trouver des Chantiers - Augmentez Votre CA | Trouver-Mon-Chantier.fr',
-  description: "Boostez votre CA avec Trouver-Mon-Chantier.fr. Solutions sur-mesure pour professionnels du bâtiment. Sans engagement ni frais cachés.",
+  title: 'Mentions légales | Trouver-Mon-Chantier.fr',
+  description: "Mension légales de Trouver-Mon-Chantier.fr",
 }
 
 export default function RootLayout({
