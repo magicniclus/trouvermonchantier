ac476203516fad77b9da0458f426d1f2c7cf486e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-21 07:50:26 +0100

    Update ButtonAction.tsx

--- src/app/components/ButtonAction.tsx
+++ src/app/components/ButtonAction.tsx
@@ -10,7 +10,7 @@ interface ButtonPhoneProps {
 const ButtonPhone: React.FC<ButtonPhoneProps> = ({ value = 'Envoyer', bgColor = "textSecondary", textColor = "white" }) => {
 
     return (
-        <button className={`py-3 lg:px-5 px-3 rounded-lg bg-textSecondary text-white flex mt-10 min-w-[150px] justify-center items-center`}>
+        <button className={`py-3 lg:px-5 px-3 rounded-lg bg-${bgColor} text-${textColor} flex mt-10 min-w-[150px] justify-center items-center`}>
             <div className='flex'>
                 {value}
             </div>
