3f692999892a64b0be8eb975d40be805313a99e6
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-22 14:52:04 +0100

    Update layout.tsx

--- src/app/merci/layout.tsx
+++ src/app/merci/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './../globals.css'
+import Analytics from './../components/Analytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="logo/favicon.png" sizes="any" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><Analytics />{children}</body>
     </html>
   )
 }
