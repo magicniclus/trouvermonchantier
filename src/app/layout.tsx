5e9cdc6e16edd93ac14e4b2851ad497792cd252d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-22 13:29:39 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,41 +1,37 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Script from 'next/script'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Trouver des Chantiers - Augmentez Votre CA | Trouver-Mon-Chantier.fr',
   description: "Boostez votre CA avec Trouver-Mon-Chantier.fr. Solutions sur-mesure pour professionnels du bâtiment. Sans engagement ni frais cachés.",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: 'GTM-WP9R782', // Remplacez par votre ID GTM
+    };
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <html lang="fr">
       <head>
         <meta charSet="utf-8" />
         <link rel="icon" href="logo/favicon.png" sizes="any" />
-         <Script
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-NTLJ2NWF');
-              `,
-        }}
-      />
       </head>
       <body className={inter.className}>
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
