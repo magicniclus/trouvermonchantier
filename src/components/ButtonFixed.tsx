5e9b0716938d23b0ab4dca8f8095b07fceecbbf1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-24 17:11:08 +0100

    Create ButtonFixed.tsx

---
+++ src/components/ButtonFixed.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ButtonFixed = () => {
+    return (
+        <a href="#form" className="py-3 lg:px-5 px-3 rounded-full lg:rounded-lg bg-yellow text-textSecondary flex fixed bottom-10 left-1/2 transform -translate-x-1/2 shadow-md hover:transition-all hover:duration-100 hover:ease-in-out hover:shadow-lg">
+            <div>
+                Contacter
+            </div>
+        </a>
+
+    );
+};
+
+export default ButtonFixed;
