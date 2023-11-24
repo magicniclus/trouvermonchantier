ddcc9eb7eb569f883f06245db830e141eff0d8a9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-24 17:10:54 +0100

    Update layout.tsx

--- src/app/merci/layout.tsx
+++ src/app/merci/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './../globals.css'
-import Analytics from './../components/Analytics'
+import Analytics from '../../components/Analytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
