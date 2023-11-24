043fe25aece751d270f541a314a550f9c37fd336
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-24 17:10:48 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Suspense } from 'react'
-import Analytics from './components/Analytics'
+import Analytics from '../components/Analytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
